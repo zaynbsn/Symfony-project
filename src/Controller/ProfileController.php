71b724f9f758d20ea3d19ce456c81e709fd18b11
zaynbsn <nils.vainui@gmail.com>
2024-03-31 00:13:45 +0100

    add profile/id route & update template

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,22 @@ class ProfileController extends AbstractController
     {
         $user = $this->getUser();
 
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/profile/{id}', name: 'app_view_profile')]
+    public function viewProfile($id, EntityManagerInterface $entityManager): Response
+    {
+        $repository = $entityManager->getRepository(User::class);
+        $user = $repository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Render the profile template with user data
         return $this->render('profile/index.html.twig', [
             'user' => $user,
         ]);
